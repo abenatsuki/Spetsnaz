5535dfe8f7d41052ad3795e128e9a0d1d0ca249d
abenatsuki <56098844+abenatsuki@users.noreply.github.com>
2019-10-09 00:13:42 +0900

    自機の縦横移動

--- Spetsnaz/Assets/Script/PlayerMove.cs
+++ Spetsnaz/Assets/Script/PlayerMove.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+
+    [SerializeField, Tooltip("自機の速度")]
+    private float speed=15.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //プレイヤーを移動させる
+        CharacterController controller = GetComponent<CharacterController>();
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
