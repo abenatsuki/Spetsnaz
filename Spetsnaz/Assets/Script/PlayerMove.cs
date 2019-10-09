d57228589eaff14420d37988fbe0727268465e3e
abe.natsuki <a.be.4966@gmail.com>
2019-10-09 13:42:26 +0900

    Rigidbodyに変更

--- Spetsnaz/Assets/Script/PlayerMove.cs
+++ Spetsnaz/Assets/Script/PlayerMove.cs
@@ -2,29 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+ [RequireComponent(typeof(Rigidbody))] //Rigidbody追加
+ [RequireComponent(typeof(Collider))]//Collider追加
+
 public class PlayerMove : MonoBehaviour
 {
 
-    [SerializeField, Tooltip("自機の速度")]
-    private float speed=15.0f;
 
-    private Vector3 moveDirection = Vector3.zero;
+    [SerializeField, Tooltip("自機の速度")]
+    float moveSpeed = 5f;
+    float footDistance;
+
+    Vector3 velocity;
+    bool isGround;
+    Rigidbody rigidbody;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        rigidbody = GetComponent<Rigidbody>();
+        footDistance = GetComponent<Collider>().bounds.min.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //プレイヤーを移動させる
-        CharacterController controller = GetComponent<CharacterController>();
 
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        moveDirection = transform.TransformDirection(moveDirection);
-        moveDirection *= speed;
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed;
 
-        controller.Move(moveDirection * Time.deltaTime);
+        velocity.x = direction.x;
+        velocity.z = direction.z;
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
     }
 }
