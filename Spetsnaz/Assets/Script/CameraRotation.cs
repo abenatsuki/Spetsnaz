5825645e24524ed2385e25757360f5383e07cc51
abe.natsuki <a.be.4966@gmail.com>
2019-10-10 11:06:16 +0900

    マウス視点移動

---
+++ Spetsnaz/Assets/Script/CameraRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    Transform verRotation;
+    Transform horRotation;
+    [SerializeField,Tooltip("カメラ感度")]
+    float sensitivity=1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        verRotation = transform.parent;//自機のtransform
+        horRotation = GetComponent<Transform>();//カメラのtransform
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xRotation = Input.GetAxis("Mouse X");
+        float yRotation = Input.GetAxis("Mouse Y");
+
+        verRotation.transform.Rotate(0, xRotation*sensitivity, 0);
+        horRotation.transform.Rotate(-yRotation*sensitivity, 0, 0);
+    }
+}
--- Spetsnaz/Assets/Script/PlayerMove.cs
+++ Spetsnaz/Assets/Script/PlayerMove.cs
@@ -7,37 +7,39 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-
-
     [SerializeField, Tooltip("自機の速度")]
     float moveSpeed = 5f;
-    float footDistance;
 
     Vector3 velocity;
     bool isGround;
-    Rigidbody rigidbody;
-
-
-
+    Rigidbody rigidbody3D;
+    
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        footDistance = GetComponent<Collider>().bounds.min.y;
+        rigidbody3D = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed;
-
+       
+        float x=Input.GetAxis("Horizontal");
+        float z= Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(x, 0, z) * moveSpeed;
+        if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Vertical") > 0)
+        {
+          
+        }
         velocity.x = direction.x;
         velocity.z = direction.z;
        
     }
     private void FixedUpdate()
     {
-        rigidbody.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
+        
+        
+        //velocity = velocity + transform.forward;
+        rigidbody3D.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
     }
 }
