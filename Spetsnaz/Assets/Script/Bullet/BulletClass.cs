a5ef44a35715fbaebe7984e03ca82a5d987033f8
Hirotaka_I <Hirotaka_ito_1021@yahoo.co.jp>
2019-10-15 15:18:07 +0900

    Bulletの種類増えただけ

---
+++ Spetsnaz/Assets/Script/Bullet/BulletClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletClass : MonoBehaviour
+{
+    abstract class BulletState //純粋仮想関数
+    {
+        public abstract void Update();
+    } 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
