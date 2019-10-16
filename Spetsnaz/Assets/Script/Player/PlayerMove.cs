a1246d914693f5a1d0a908ca0158e1931c0a2886
abe.natsuki <a.be.4966@gmail.com>
2019-10-16 13:46:18 +0900

    梯子あたり判定

--- Spetsnaz/Assets/Script/Player/PlayerMove.cs
+++ Spetsnaz/Assets/Script/Player/PlayerMove.cs
@@ -1,50 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public enum PlayerStateEnum
 {
     IDLE = 0,
-    WORK,
-    DASH,
-    EIM,
+    WORK,//歩き
+    DASH,//ダッシュ
+    EIM,//エイム
+    CLIMB,//梯子上り
+    DOWN,//梯子下り
+
 };
 
- [RequireComponent(typeof(Rigidbody))] //Rigidbody追加
- [RequireComponent(typeof(Collider))]//Collider追加
+[RequireComponent(typeof(Rigidbody))] //Rigidbody追加
+[RequireComponent(typeof(Collider))]//Collider追加
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField, Tooltip("自機の歩きの速さ")]
-    float workSpeed=.0f;
+    float workSpeed = .0f;
     [SerializeField, Tooltip("自機の走る速さ")]
-    float dashSpeed=.0f;
+    float dashSpeed = .0f;
     [SerializeField, Tooltip("自機がエイム時の移動速度")]
     float eimSpeed = .0f;
 
-    public bool dashFlag {  get;private set; }
-    public PlayerStateEnum playerState { get;private set; }//自機の状態
+    public bool shotFlag { get; private set; }//撃っているかどうか
+    public PlayerStateEnum playerState { get; private set; }//自機の状態
     Vector3 velocity;
     Rigidbody rigidbody3D;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        dashFlag = false;
+        shotFlag = false;//弾を撃っているかフラグ
         rigidbody3D = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         velocity = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized;
 
         if (Input.GetMouseButton(1))
         {
             playerState = PlayerStateEnum.EIM;
         }
-       else if (Input.GetKey(KeyCode.LeftShift))
+        else if (Input.GetKey(KeyCode.LeftShift))
         {
             playerState = PlayerStateEnum.DASH;
         }
@@ -56,23 +56,68 @@ public class PlayerMove : MonoBehaviour
         switch (playerState)
         {
             case PlayerStateEnum.WORK://歩き
-            velocity*= workSpeed;
+                WorkUpdate();
                 break;
             case PlayerStateEnum.EIM://エイム
-                velocity *= eimSpeed;
+                EimUpdate();
                 break;
             case PlayerStateEnum.DASH://ダッシュ
-                velocity *= dashSpeed;
+                DashUpdate();
                 break;
-
+            case PlayerStateEnum.CLIMB://梯子上り
+                ClimbUpdate();
+                break;
+            case PlayerStateEnum.DOWN://梯子下り
+                DownUpdate();
+                break;
         }
-           
-        
     }
     private void FixedUpdate()
     {
-         rigidbody3D.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
+        rigidbody3D.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
     }
 
+    private void WorkUpdate()
+    {
+        if (!shotFlag)
+            LadderHitCheck();
+
+        velocity *= workSpeed;
+    }
+    private void EimUpdate()
+    {
+        velocity *= eimSpeed;
+    }
+    private void DashUpdate()
+    {
+        if (!shotFlag)
+            LadderHitCheck();
+
+        velocity *= dashSpeed;
+    }
+    private void ClimbUpdate()
+    {
+
+    }
+    private void DownUpdate()
+    {
+
+    }
+    private void LadderHitCheck()
+    {
+
+
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            Debug.Log("当たった");
+            
+        }
+        
+
+    }
+    
 
 }
