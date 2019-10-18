dad83c70b2f12afe126af8f11278135c7b9d0ec6
Hirotaka_I <Hirotaka_ito_1021@yahoo.co.jp>
2019-10-18 13:37:31 +0900

    単発調整中

--- Spetsnaz/Assets/Script/Bullet/Bullet_Semi.cs
+++ Spetsnaz/Assets/Script/Bullet/Bullet_Semi.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bullet_Semi : MonoBehaviour
 {
+    GameObject Player;
+    PlayerDataProvider GetPlayerDataProvider;
     public GameObject Bullet;
 
     private float shotcooltimer; //再度弾を打つためのカウントタイム
@@ -21,9 +23,17 @@ public class Bullet_Semi : MonoBehaviour
         // タイマーの時間を動かす
         shotcooltimer += Time.deltaTime;
 
-        //弾の発射
+        //弾の発射 エイム時
         if (Input.GetMouseButtonDown(0) && ammocnt >0)
         {
+            ammocnt--;
+            shotcooltimer = 0.0f;
+            Instantiate(Bullet, transform.position, transform.rotation);
+        }
+        //腰うち
+        else if(Input.GetMouseButtonDown(0) && ammocnt > 0)
+        {
+            ammocnt--;
             shotcooltimer = 0.0f;
             Instantiate(Bullet, transform.position, transform.rotation);
         }
