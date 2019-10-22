b1e7c2daba8887f75cad1befc864626d89f37cc0
Hirotaka_I <Hirotaka_ito_1021@yahoo.co.jp>
2019-10-22 15:18:08 +0900

    見ている方向に弾が飛ぶように
    プレイヤー情報の取得に躓き中

--- Spetsnaz/Assets/Script/Bullet/Bullet_Semi.cs
+++ Spetsnaz/Assets/Script/Bullet/Bullet_Semi.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class Bullet_Semi : MonoBehaviour
 {
-    GameObject Player;
-    PlayerDataProvider GetPlayerDataProvider;
+    GameObject player;
+    PlayerDataProvider script;
+
     public GameObject Bullet;
     public GameObject Muzzle;
 
     private int ammocnt; //残弾数
-
+    PlayerStateEnum playerStateEnum;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
+        script = GetComponent<PlayerDataProvider>();
         ammocnt = 8;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        playerStateEnum = script.IsPlayerStateEnum;
+        Debug.Log(playerStateEnum);
         //弾の発射 エイム時
-        if (Input.GetMouseButtonDown(0) && ammocnt >0)
+        if (Input.GetMouseButtonDown(0) && ammocnt > 0 /*&& playerStateEnum == PlayerStateEnum.EIM*/)
         {
             ammocnt--;
-            Instantiate(Bullet, Muzzle.transform.position,transform.rotation);
+            Instantiate(Bullet, Muzzle.transform.position, transform.rotation);
         }
+        //腰うち
+        //if (Input.GetMouseButtonDown(0) && ammocnt > 0)
+        //{
+        //    ammocnt--;
+        //    Instantiate(Bullet, Muzzle.transform.position, transform.rotation);
+        //}
 
         //弾のリロード
         if (Input.GetKeyDown(KeyCode.R))
