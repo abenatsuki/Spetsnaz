ea392537771630860b59e29f09f2616f9a2202cd
Hirotaka_I <Hirotaka_ito_1021@yahoo.co.jp>
2019-11-05 15:18:34 +0900

    銃のリロード処理
    アニメーションを付ければ完璧

--- Spetsnaz/Assets/Script/Bullet/BulletMove.cs
+++ Spetsnaz/Assets/Script/Bullet/BulletMove.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
+    public bool reaction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        reaction = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         transform.position += transform.forward * Time.deltaTime *100;
 
         //発射した弾を３秒後に削除する。
--- Spetsnaz/Assets/Script/Bullet/Bullet_Semi.cs
+++ Spetsnaz/Assets/Script/Bullet/Bullet_Semi.cs
@@ -6,12 +6,14 @@ public class Bullet_Semi : MonoBehaviour
 {
     PlayerDataProvider script;
     GameObject player;
-    
 
     public GameObject Bullet;
     public GameObject Muzzle;
 
     private int ammocnt; //残弾数
+
+    private float ReloadTime;
+
     PlayerStateEnum playerStateEnum;
     // Start is called before the first frame update
     void Start()
@@ -19,31 +21,36 @@ public class Bullet_Semi : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");//タグでオブジェクトを見つける
         script = player.GetComponent<PlayerDataProvider>();//Playerオブジェクトからスクリプトを持ってくる
         ammocnt = 8;
+        ReloadTime = 120;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //リロードできるまでの時間
+        ReloadTime--;
 
         playerStateEnum = script.IsPlayerStateEnum;//プレイヤーのステータスを代入
         //Debug.Log(playerStateEnum);//プレイヤーの状態見たいときはつかってね
         //弾の発射 エイム時
-        if (Input.GetMouseButtonDown(0) && ammocnt > 0 && playerStateEnum == PlayerStateEnum.EIM)
+        if (Input.GetMouseButtonDown(0) && ammocnt > 0 && ReloadTime < 0 && playerStateEnum == PlayerStateEnum.EIM)
+        {
+            ammocnt--;
+            Instantiate(Bullet, Muzzle.transform.position, transform.rotation);
+
+        }
+        //腰うち
+        else if (Input.GetMouseButtonDown(0) && ammocnt > 0 && ReloadTime < 0 )
         {
             ammocnt--;
             Instantiate(Bullet, Muzzle.transform.position, transform.rotation);
         }
-        //腰うち
-        //if (Input.GetMouseButtonDown(0) && ammocnt > 0)
-        //{
-        //    ammocnt--;
-        //    Instantiate(Bullet, Muzzle.transform.position, transform.rotation);
-        //}
 
         //弾のリロード
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && ReloadTime < 0 && ammocnt < 8)
         {
             ammocnt = 8;
+            ReloadTime = 120;
         }
 
     }
