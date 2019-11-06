4faea29310e92734e794f112a64073e6aeeffcd8
abe.natsuki <a.be.4966@gmail.com>
2019-11-06 15:41:28 +0900

    ステージの大きさ調整

---
+++ Spetsnaz/Assets/Script/Ui/TargetCount.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetCount : MonoBehaviour
+{
+    
+    private int targetCount;
+    public Image[] image;
+    [SerializeField]
+    private Sprite[] numberFont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var counter in image)
+        {
+            counter.sprite = numberFont[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
