f363d5766143a800f2a11e99d22ad29da08ceb75
abe.natsuki <a.be.4966@gmail.com>
2019-11-12 13:57:51 +0900

    Ui実装

--- Spetsnaz/Assets/Script/Ui/ScoreCount.cs
+++ Spetsnaz/Assets/Script/Ui/ScoreCount.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +8,8 @@ public class ScoreCount : MonoBehaviour
     private List<Image> image = new List<Image>();
     [SerializeField]
     private List<Sprite> numberFont = new List<Sprite>();
+
+    int score = 123456;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +22,14 @@ public class ScoreCount : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        image[0].sprite = numberFont[score % 10];
+        image[1].sprite = numberFont[(score / 10) % 10];
+        image[2].sprite = numberFont[(score / 100) % 10];
+        image[3].sprite = numberFont[(score / 1000) % 10];
+        image[4].sprite = numberFont[(score / 10000) % 10];
+        image[5].sprite = numberFont[score / 100000];
+
     }
+
+  
 }
