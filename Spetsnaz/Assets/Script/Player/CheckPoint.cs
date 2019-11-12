76f686ea05999850ef7d9f93555c4856fed17ea3
abe.natsuki <a.be.4966@gmail.com>
2019-11-12 15:39:07 +0900

    Scoreじわっと表示

---
+++ Spetsnaz/Assets/Script/Player/CheckPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public bool checkPointFlag  { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkPointFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CheckPoint")
+        {
+            checkPointFlag = true;
+            Debug.Log("当たった");
+        }
+    }
+
+}
--- Spetsnaz/Assets/Script/Player/PlayerDataProvider.cs
+++ Spetsnaz/Assets/Script/Player/PlayerDataProvider.cs
@@ -3,6 +3,8 @@
 public class PlayerDataProvider : MonoBehaviour
 {
     [SerializeField] private PlayerMove playerMove=null;
+    [SerializeField] private CheckPoint checkPoint = null;
 
     public PlayerStateEnum IsPlayerStateEnum { get { return playerMove.playerState; } }//自機の状態
+    public bool IsCheckPointFlag { get { return checkPoint.checkPointFlag; } }
 }
