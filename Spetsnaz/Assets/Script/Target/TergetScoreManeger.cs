2982cdd406813088894016bfac9748ad2cb574a5
Hirotaka_I <Hirotaka_ito_1021@yahoo.co.jp>
2019-11-21 11:17:38 +0900

    スコア調節中だぜ！いえーーーい！

--- Spetsnaz/Assets/Script/Target/TergetScoreManeger.cs
+++ Spetsnaz/Assets/Script/Target/TergetScoreManeger.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class TergetScoreManeger : MonoBehaviour
 {
+    //定数
+    const int TS = 500000;
 
-    public int Score { get;  set; } //スコア
+    public int Score { get;  set; } //ターゲットのスコア
+    public int TimeScore { get; private set; }//時間によるスコア配当
+    public float TimecntUp; //プレイ時間
+    public int targetcnt { get; set; }
     
     // Start is called before the first frame update
     void Start()
     {
-
+        Score = 0;
+        TimeScore = TS;
+        TimecntUp = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Score);
+        Debug.Log(TimecntUp);
     }
 }
