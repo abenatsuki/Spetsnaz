0d0c8676f56b0e4b0772448a66c3112acc522681
abe.natsuki <a.be.4966@gmail.com>
2019-11-22 14:02:34 +0900

    エイム切り替えアニメーション実装

--- Spetsnaz/Assets/Script/Player/PlayerAnimation.cs
+++ Spetsnaz/Assets/Script/Player/PlayerAnimation.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    GameObject player;
+    PlayerDataProvider playerScript;
     Animator playerAnimator;
+
+    PlayerStateEnum playerState;
+
     // Start is called before the first frame update
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
-        playerAnimator.SetInteger("Param", 0);
+        playerAnimator.SetInteger("PlayerState", 0);
+
+        player = GameObject.FindGameObjectWithTag("Player");//タグでオブジェクトを見つける
+        playerScript = player.GetComponent<PlayerDataProvider>();//Playerオブジェクトからスクリプトを持ってくる
+        playerState = playerScript.IsPlayerStateEnum;
+        if (playerScript == null)
+            Debug.Log("error");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerState = playerScript.IsPlayerStateEnum;
+        switch (playerState)
+        {
+            case PlayerStateEnum.WORK://歩き
+                playerAnimator.SetInteger("PlayerState", 0);
+                break;
+            case PlayerStateEnum.EIM://エイム
+                playerAnimator.SetInteger("PlayerState", 3);
+                break;
+            case PlayerStateEnum.DASH://ダッシュ
+          
+                break;
+            case PlayerStateEnum.GRABBING://梯子つかんでいる
+             
+                break;
+        }
+
+
+
     }
 }
