568e905f7597a00736edebb815e869df4fe44851
abe.natsuki <a.be.4966@gmail.com>
2019-11-25 15:34:39 +0900

    エイム時ズームする機能

---
+++ Spetsnaz/Assets/Script/Camera/CameraFiledOfView.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFiledOfView : MonoBehaviour
+{
+    const float zoomDefault=78.0f;
+    Camera camera;
+    GameObject player;
+    PlayerDataProvider playerScript;
+    PlayerStateEnum playerState;
+
+    [SerializeField,Tooltip("どれぐらいズームするかの値")]
+    float zoomMaximam;
+    [SerializeField,Tooltip("ズームする速度")]
+    float zoomSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player");//タグでオブジェクトを見つける
+        playerScript = player.GetComponent<PlayerDataProvider>();//Playerオブジェクトからスクリプトを持ってくる
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerState = playerScript.IsPlayerStateEnum;
+
+      
+
+
+        if (playerState==PlayerStateEnum.EIM)
+        {
+            //camera.fieldOfView = 50;
+            if (camera.fieldOfView >= zoomMaximam)
+            {
+                camera.fieldOfView -= zoomSpeed;
+            }
+            
+        }
+        else
+        {
+            if (camera.fieldOfView <= zoomDefault)
+            {
+                camera.fieldOfView += zoomSpeed;
+            }
+            //camera.fieldOfView = zoomDefault;
+        }
+    }
+}
