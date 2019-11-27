24f95611a756aeddf2468f22d59c1a76d2f42642
Hirotaka_I <Hirotaka_ito_1021@yahoo.co.jp>
2019-11-27 13:05:02 +0900

    Targetいろいろ完成

--- Spetsnaz/Assets/Script/Target/TergetScoreManeger.cs
+++ Spetsnaz/Assets/Script/Target/TergetScoreManeger.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class TergetScoreManeger : MonoBehaviour
 {
-    [SerializeField]
-    public int TargetMax;
     public int Score { get;  set; } //スコア
-    
     public int TargetCnt { get; set; } //ターゲットの数
-   
+    public int TargetMax { get; private set; } //ターゲットの数(定数) 
+
+    bool startflg;//定数を代入するためのフラグ
+    bool clearflg; //クリアしたかどうか
 
     // Start is called before the first frame update
     void Start()
-    { 
-
+    {
+        startflg = false;
+        clearflg =false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!startflg)
+        {
+            TargetMax = TargetCnt;
+            startflg = true;
+        }
+        if(TargetCnt==0)
+        {
+            clearflg = true;
+        }
         Debug.Log(TargetMax);
 
         Debug.Log(Score);
