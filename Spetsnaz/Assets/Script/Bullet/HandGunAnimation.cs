e1a5530d470cab056ecf6735921eb2e8eda3201d
abe.natsuki <a.be.4966@gmail.com>
2019-12-04 15:36:53 +0900

    銃アニメーション試行錯誤

---
+++ Spetsnaz/Assets/Script/Bullet/HandGunAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGunAnimation : MonoBehaviour
+{
+    GameObject player;
+    PlayerDataProvider playerScript;
+    Animator handGunAnimator;
+
+    PlayerStateEnum playerState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        handGunAnimator = GetComponent<Animator>();
+        handGunAnimator.SetInteger("HandGunState", 0);
+
+        player = GameObject.FindGameObjectWithTag("Player");//タグでオブジェクトを見つける
+        playerScript = player.GetComponent<PlayerDataProvider>();//Playerオブジェクトからスクリプトを持ってくる
+        playerState = playerScript.IsPlayerStateEnum;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerState = playerScript.IsPlayerStateEnum;
+        switch (playerState)
+        {
+          
+            case PlayerStateEnum.RELOAD:
+                handGunAnimator.SetInteger("HandGunState", 1);
+                break;
+               
+        }
+
+
+
+    }
+}
---
+++ Spetsnaz/Assets/Script/Scene/MoveWeaponSelectScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveWeaponSelectScene : MonoBehaviour
+{
+   public void StandardOnClick()
+    {
+        DontDestroyObjectManager.DestoryAll();
+        SceneManager.LoadScene("WeaponSelectScene");
+    }
+}
