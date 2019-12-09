ab1d43c70abd8918b97f3226b1f5d1078b22a91d
abe.natsuki <a.be.4966@gmail.com>
2019-12-09 15:25:10 +0900

    テクスチャ張り

---
+++ Spetsnaz/Assets/Script/Player/ChangeWeapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWeapon : MonoBehaviour
+{
+    [SerializeField,Tooltip("武器")]
+    List<GameObject> weapon = new List<GameObject>();
+    int num;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        num = 0;
+        var we = Instantiate<GameObject>(weapon[num]);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void changeWeapon()
+    {
+        
+    }
+}
