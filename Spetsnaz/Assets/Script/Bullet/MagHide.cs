b929025ba17c193810a2e9f76fa917942530c4e9
abe.natsuki <a.be.4966@gmail.com>
2019-12-12 15:19:42 +0900

    Magの透明化

---
+++ Spetsnaz/Assets/Script/Bullet/MagHide.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagHide : MonoBehaviour
+{
+    MeshRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        //renderer.material.color = new Color(0, 0, 0, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
