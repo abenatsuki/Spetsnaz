dfc2b196eb43ebc88fae79c729696c7fe4da0029
abe.natsuki <a.be.4966@gmail.com>
2019-12-13 15:22:05 +0900

    シェーダー実験

--- Spetsnaz/Assets/Script/Bullet/MagHide.cs
+++ Spetsnaz/Assets/Script/Bullet/MagHide.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class MagHide : MonoBehaviour
 {
     MeshRenderer renderer;
+    Color color;
+    int count;
     // Start is called before the first frame update
     void Start()
     {
-        renderer = GetComponent<MeshRenderer>();
-        //renderer.material.color = new Color(0, 0, 0, 1.0f);
+        count = 0;
+        color = GetComponent<Renderer>().material.color;
+        color.a = 1.0f;
+        gameObject.GetComponent<Renderer>().material.color = color;
+       
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        count++;
+        if (count > 600)
+        {
+            color.a = 0.0f;
+            gameObject.GetComponent<Renderer>().material.color = color;
+        }
+        if (count > 700)
+        {
+            color.a = 1.0f;
+            gameObject.GetComponent<Renderer>().material.color = color;
+        }
         
     }
 }
