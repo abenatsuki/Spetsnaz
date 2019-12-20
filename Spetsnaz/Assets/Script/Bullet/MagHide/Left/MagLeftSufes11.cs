9d38070a535ad285846f94c81caf0f5aa6af701e
abe.natsuki <a.be.4966@gmail.com>
2019-12-20 13:53:10 +0900

    アニメーションごり押し失敗

--- Spetsnaz/Assets/Script/Bullet/MagHide/Left/MagLeftSufes11.cs
+++ Spetsnaz/Assets/Script/Bullet/MagHide/Left/MagLeftSufes11.cs
@@ -2,28 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class MagHide : MonoBehaviour
+public class MagLeftSufes11 : MonoBehaviour
 {
     GameObject player;
     PlayerDataProvider playerScript;
     Animator playerAnimator;
 
-    bool inFlag=false;
+    bool inFlag = false;
     Color color;
     int reloadFrame;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
         color = GetComponent<Renderer>().material.color;
         color.a = 1.0f;
         gameObject.GetComponent<Renderer>().material.color = color;
         player = GameObject.FindGameObjectWithTag("Player");//タグでオブジェクトを見つける
-        
+
         playerScript = player.GetComponent<PlayerDataProvider>();//Playerオブジェクトからスクリプトを持ってくる
-       
+
         //透明に
         color.a = 0.0f;
         gameObject.GetComponent<Renderer>().material.color = color;
@@ -36,17 +35,25 @@ public class MagHide : MonoBehaviour
 
         reloadFrame = playerScript.IsPlayerReloadFrame;
 
+       
 
         if (reloadFrame > 60)
         {
             color.a = 1.0f;
             gameObject.GetComponent<Renderer>().material.color = color;
         }
-      
+        if (reloadFrame > 72||reloadFrame==0)
+        {
+            color.a = 0.0f;
+            gameObject.GetComponent<Renderer>().material.color = color;
+        }
+ if (reloadFrame  >90)
+        {
+            //透明に
+            color.a = 0.0f;
+            gameObject.GetComponent<Renderer>().material.color = color;
+        }
 
     }
-   
-    
-
 
 }
