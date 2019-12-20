9b7fe8c3ae1335ab723d32abec9f79026672894c
abe.natsuki <a.be.4966@gmail.com>
2019-12-20 11:40:44 +0900

    Revert "アニメーション試行錯誤"
    This reverts commit 86c3018adc5232a808192a038a82a149c5c8d232.

--- Spetsnaz/Assets/Script/Bullet/MagHide.cs
+++ Spetsnaz/Assets/Script/Bullet/MagHide.cs
@@ -2,27 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagLeftSufes11 : MonoBehaviour
+
+public class MagHide : MonoBehaviour
 {
     GameObject player;
     PlayerDataProvider playerScript;
     Animator playerAnimator;
 
-    bool inFlag = false;
+    bool inFlag=false;
     Color color;
     int reloadFrame;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
         color = GetComponent<Renderer>().material.color;
         color.a = 1.0f;
         gameObject.GetComponent<Renderer>().material.color = color;
         player = GameObject.FindGameObjectWithTag("Player");//タグでオブジェクトを見つける
-
+        
         playerScript = player.GetComponent<PlayerDataProvider>();//Playerオブジェクトからスクリプトを持ってくる
-
+       
         //透明に
         color.a = 0.0f;
         gameObject.GetComponent<Renderer>().material.color = color;
@@ -35,25 +36,17 @@ public class MagLeftSufes11 : MonoBehaviour
 
         reloadFrame = playerScript.IsPlayerReloadFrame;
 
-       
 
         if (reloadFrame > 60)
         {
             color.a = 1.0f;
             gameObject.GetComponent<Renderer>().material.color = color;
         }
-        if (reloadFrame > 72||reloadFrame==0)
-        {
-            color.a = 0.0f;
-            gameObject.GetComponent<Renderer>().material.color = color;
-        }
- if (reloadFrame  >100)
-        {
-            //透明に
-            color.a = 0.0f;
-            gameObject.GetComponent<Renderer>().material.color = color;
-        }
+      
 
     }
+   
+    
+
 
 }
