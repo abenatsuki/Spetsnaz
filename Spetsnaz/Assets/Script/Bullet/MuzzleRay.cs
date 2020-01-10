4696adfea50e8ad555be0e44df56071eb3e0373d
abe.natsuki <a.be.4966@gmail.com>
2020-01-10 15:37:45 +0900

    エイムずれ修正
    エイム中だけ適用予定

--- Spetsnaz/Assets/Script/Bullet/MuzzleRay.cs
+++ Spetsnaz/Assets/Script/Bullet/MuzzleRay.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArmRotation : MonoBehaviour
+public class MuzzleRay : MonoBehaviour
 {
-    private GameObject muzzle;
-    
+    public Vector3 position;
+
     // Start is called before the first frame update
     void Start()
     {
-        muzzle = GameObject.FindGameObjectWithTag("GazePoint");
-        Debug.Log(muzzle);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-         this.transform.LookAt(muzzle.transform.position);
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        position = ray.GetPoint(1000);
+
     }
 }
