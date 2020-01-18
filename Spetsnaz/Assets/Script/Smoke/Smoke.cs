b090a10feecee00a358af08d18c4270e569dcb0d
abe.natsuki <a.be.4966@gmail.com>
2020-01-19 02:36:58 +0900

    smoke実装

---
+++ Spetsnaz/Assets/Script/Smoke/Smoke.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smoke : MonoBehaviour
+{
+    [SerializeField]
+    GameObject particlePrefab;
+
+    bool OutBreakFlag=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision _collision)
+    {
+        if (_collision.gameObject.tag == "Stage"&&!OutBreakFlag)
+        {
+           
+           var obj= Instantiate(particlePrefab, transform.position, transform.rotation);
+            obj.transform.parent = transform;
+            OutBreakFlag = true;
+        }
+    }
+}
---
+++ Spetsnaz/Assets/Script/Smoke/SmokeSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeSpawner : MonoBehaviour
+{
+    
+    [SerializeField]
+    float throwSpeed;
+    GameObject smokeGrenade;
+    bool SpawnFlag = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smokeGrenade =(GameObject)Resources.Load("SmokeGrenade");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            var obj= Instantiate(smokeGrenade, transform.position, transform.rotation);
+            obj.transform.parent = transform;
+            var smokeRigidBody = obj.GetComponent<Rigidbody>();
+            smokeRigidBody.AddForce(transform.forward * throwSpeed);
+        }
+       
+    }
+}
