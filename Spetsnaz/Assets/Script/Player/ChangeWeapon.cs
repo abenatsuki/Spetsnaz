adc4d71a0108adcc87c1cec32bd448d545f6fb8c
abe.natsuki <a.be.4966@gmail.com>
2020-01-22 15:36:42 +0900

    調整

--- Spetsnaz/Assets/Script/Player/ChangeWeapon.cs
+++ Spetsnaz/Assets/Script/Player/ChangeWeapon.cs
@@ -1,34 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
+
+enum Now_Weapon
+{
+    Assult_Rifle = 0,
+    Hand_Gun
+}
 
 public class ChangeWeapon : MonoBehaviour
 {
-    [SerializeField,Tooltip("腕モデル")]
+    [SerializeField, Tooltip("腕モデル")]
     List<GameObject> weapon = new List<GameObject>();
-   
 
-
-
+    Now_Weapon nowWeapon;
 
     // Start is called before the first frame update
     void Start()
     {
-       
-
-
-
+        nowWeapon = Now_Weapon.Assult_Rifle;
+        weapon[1].SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            changeWeapon();
+        }
+
     }
 
     void changeWeapon()
     {
-        
+        if (nowWeapon == Now_Weapon.Assult_Rifle)
+        {
+            nowWeapon = Now_Weapon.Hand_Gun;
+            weapon[(int)Now_Weapon.Assult_Rifle].SetActive(false);
+            weapon[(int)Now_Weapon.Hand_Gun].SetActive(true);
+        }
+        else if (nowWeapon == Now_Weapon.Hand_Gun)
+        {
+            nowWeapon = Now_Weapon.Assult_Rifle;
+            weapon[(int)Now_Weapon.Hand_Gun].SetActive(false);
+            weapon[(int)Now_Weapon.Assult_Rifle].SetActive(true);
+        }
+
+
     }
 }
